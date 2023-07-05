bf15512b4c9d5058561a93c5e1d1e2c63ec469a0
Ayush Chopra <ayushchopra@Ayushs-MacBook-Pro.local>
2023-07-05 16:29:55 -0400

    nca model

--- AgentTorch/models/nca/simulator.py
+++ AgentTorch/models/nca/simulator.py
@@ -1,12 +1,42 @@
 '''Define simulator structure in this file'''
 
-from AgentTorch.config import Configurator
+from AgentTorch import Runner, Registry, Configurator
 from substeps import evolve_cell
 
-config = Configurator()
+class NCARunner(Runner):
+    def __init__(self, **args, **kwargs):
+        super().__init__(**args, **kwargs)
 
-# simulate metadata
+    def _nca_initialize_state(self, shape, params):
+        device = torch.device(params['device'])
+        batch_size = params['batch_size']
+        n_channels = int(params['n_channels'].item())
+        processed_shape = [process_shape_omega(s) for s in shape]
+        grid_shape = [np.sqrt(processed_shape[0]).astype(int), np.sqrt(processed_shape[0]).astype(int), processed_shape[1]]
+        seed_x = np.zeros(grid_shape, np.float32)
+        seed_x[grid_shape[0]//2, grid_shape[1]//2, 3:] = 1.0
+        x0 = np.repeat(seed_x[None, ...], batch_size, 0)
+        x0 = torch.from_numpy(x0.astype(np.float32)).to(device)
+        return x0
 
-# state properties and initializers
+    def reset(self):
+        shape = [5184, 16]
+        params = {'n_channels': torch.tensor([16.]), 'batch_size': torch.tensor([8.]), 'device': 'cpu'}
+        x0 = self._nca_initialize_state(shape, params)
+        self.state = self.initializer.state
+        self.state['agents']['automata']['cell_state'] = x0
 
-# substep definition
+
+def get_registry():
+    reg = Registry()
+
+    from substeps.evolve_cell.transition import NCAEvolve
+    reg.register(NCAEvolve, "NCAEvolve", key="transition")
+
+    from AgentTorch.helpers.environment import grid_network
+    reg.register(grid_network, "grid", key="network")
+    
+    from substeps.utils import nca_initialize_state
+    reg.register(nca_initialize_state, "nca_initialize_state", key="initialization")
+    
+    return reg
