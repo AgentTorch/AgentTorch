2ee1446126d77a1b07092f8808767256ca91da8f
Ayush Chopra <ayushchopra@Ayushs-MacBook-Pro.local>
2023-07-05 16:00:31 -0400

    deprecate unncessary utils

--- utils/observation/__init__.py
+++
@@ -1,9 +0,0 @@
-import torch
-import torch.nn as nn
-import numpy as np
-
-
-class ObservationHelper(nn.Module):
-    def __init__(self):
-        pass
-    
--- utils/transition/__init__.py
+++
@@ -1,1 +0,0 @@
-from utils.transition.opinion import *
