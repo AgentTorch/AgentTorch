36f526f3f191298034a32dabf130d4aa1f3d703b
Ayush Chopra <ayushchopra@Ayushs-MacBook-Pro.local>
2023-07-11 16:57:42 -0400

    fixed helpers

--- AgentTorch/helpers/soft.py
+++ AgentTorch/helpers/soft.py
@@ -1,6 +1,5 @@
 '''Soft Approximations of functions used for AgentTorch modules'''
 import torch
-'''rewriting hard functions with smooth approximations'''
 
 def compare(a, b):
     def compare_soft(a, b, hardness=0.8):
@@ -13,12 +12,56 @@ def compare(a, b):
     return compare_hard(a, b) + soft - soft.detach()
 
 def max(a, b):
-    return a*compare(a, b) + b*compare(a, b)
+    return a*compare(a, b) + b*compare(b, a)
+
+
+def logical_not(a, grad=True):
+    def hard_not(a):
+        assert a.dtype == torch.long
+        return torch.logical_not(a)
+    
+    def soft_not(a):
+        return 1 - a
+    
+    if not grad:
+        return hard_not(a.long())
+
+    soft = soft_not(a)
+    return hard_not(a.long()) + (soft - soft.detach())
+
+def logical_or(a, b, grad=True):
+    def hard_or(a, b):
+        assert a.dtype == torch.long and b.dtype == torch.long
+        return torch.logical_or(a, b)
+    
+    def soft_or(a, b):
+        return a + b
+    
+    if not grad:
+        return hard_or(a.long(), b.long())
+
+    soft = soft_or(a, b)
+    return hard_or(a.long(), b.long()) + (soft - soft.detach())
+
+def logical_and(a, b, grad=True):
+    def hard_and(a, b):
+        assert a.dtype == torch.long and b.dtype == torch.long
+        return torch.logical_and(a, b)
+    
+    def soft_and(a, b):
+        return a * b
+    
+    if not grad:
+        return hard_and(a.long(), b.long())
+
+    soft = soft_and(a, b)
+    return hard_and(a.long(), b.long()) + (soft - soft.detach())
 
 def discrete_sample(sample_prob, size, hard=True):
     probs = sample_prob * torch.ones(size)
-    probs = torch.vstack((probs, 1.0 - probs)).transpose(0, 1)
-    sampled_output = torch.nn.functional.gumbel_softmax(probs.log(),
+    probs = torch.hstack((probs, 1 - probs))
+    prob_logits = torch.log(probs+1e-9)
+    sampled_output = torch.nn.functional.gumbel_softmax(prob_logits,
                                                         tau=0.1,
-                                                        hard=hard)[:, 0]
+                                                        hard=hard, dim=1)[:, 0]
     return sampled_output
