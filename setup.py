fa2ecab1bdb18df8faa8dfa61508415172f29081
Ayush Chopra <ayushchopra@Ayushs-MacBook-Pro.local>
2023-08-23 12:11:09 -0400

    updates

---
+++ setup.py
@@ -0,0 +1,30 @@
+
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+from os.path import abspath, dirname, join
+
+this_dir = abspath(dirname(__file__))
+with open(join(this_dir, "LICENSE")) as f:
+    license = f.read()
+
+with open(join(this_dir, "README.md"), encoding="utf-8") as file:
+    long_description = file.read()
+
+with open(join(this_dir, "requirements.txt")) as f:
+    requirements = f.read().split("\n")
+
+
+setup(
+    name="AgentTorch",
+    version="0.1.0",
+    description="Design, Simulate and Optimize Agent-based Models with Automatic Differentiation.",
+    url="https://github.com/AgentTorch/AgentTorch",
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    author="Ayush Chopra and Jayakumar Subramanian",
+    author_email="ayushc@mit.edu",
+    license="MIT License",
+    install_requires=requirements,
+    packages=find_packages(exclude=["docs"]),
+    include_package_data=True,
+)
