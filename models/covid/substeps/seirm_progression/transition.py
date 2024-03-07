fdaae9e1b93e59fa6187b939fdc886e668dbdbb5
Ayush Chopra <ayushchopra96@gmail.com>
2024-03-07 00:10:10 +0000

    consistent time updates

--- models/covid/substeps/seirm_progression/transition.py
+++ models/covid/substeps/seirm_progression/transition.py
@@ -4,12 +4,12 @@ import torch.nn.functional as F
 import re
 
 from AgentTorch.substep import SubstepTransition
-from AgentTorch.helpers import get_by_path
+from AgentTorch.helpers import get_by_path, logical_and, logical_or
 
 class SEIRMProgression(SubstepTransition):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-            
+        
         self.device = self.config['simulation_metadata']['device']
         
         self.SUSCEPTIBLE_VAR = self.config['simulation_metadata']['SUSCEPTIBLE_VAR']
@@ -22,42 +22,34 @@ class SEIRMProgression(SubstepTransition):
         self.STAGE_UPDATE_VAR = 1
         
         self.INFINITY_TIME = self.config['simulation_metadata']['num_steps_per_episode'] + 20
+        self.INFECTED_TO_RECOVERED_TIME = self.config['simulation_metadata']['INFECTED_TO_RECOVERED_TIME']
 
     def update_current_stages(self, t, current_stages, current_transition_times):
-        transit_agents = (current_transition_times > t)*self.STAGE_SAME_VAR + (current_transition_times<= t)*self.STAGE_UPDATE_VAR
-        stage_transition = (current_stages == self.EXPOSED_VAR)*transit_agents + (current_stages == self.INFECTED_VAR)*transit_agents
-
+        transition_eligible_agents = (current_transition_times <= t)
+        
+        stage_transition = transition_eligible_agents*torch.logical_or(current_stages == self.EXPOSED_VAR, current_stages == self.INFECTED_VAR)
+        
         new_stages = current_stages + stage_transition
-
         return new_stages
     
-    def update_times(self, t, next_stages, current_transition_times, infected_to_recovered_time):
-        stages = torch.clone(next_stages).long()
-        time_transition = (stages==self.INFECTED_VAR)*(current_transition_times == t)*self.INFINITY_TIME + (stages == self.EXPOSED_VAR)*(current_transition_times == t)*(infected_to_recovered_time)
-
+    def update_next_transition_times(self, t, current_stages, current_transition_times):
+        time_transition = torch.logical_and(current_stages==self.INFECTED_VAR, current_transition_times == t)*self.INFINITY_TIME + torch.logical_and(current_stages==self.EXPOSED_VAR, current_transition_times==t)*self.INFECTED_TO_RECOVERED_TIME
+        
         new_transition_times = current_transition_times + time_transition
-
         return new_transition_times
-
-    def update_times_legacy(self, t, next_stages, current_transition_times, exposed_to_infected_time, infected_to_recovered_time):   
-        new_transition_times = torch.clone(current_transition_times)
-        stages = torch.clone(next_stages).long()
-        
-        new_transition_times[(stages==self.INFECTED_VAR)*(current_transition_times == t)] = self.INFINITY_TIME
-        new_transition_times[(stages==self.EXPOSED_VAR)*(current_transition_times == t)] = t + exposed_to_infected_time
-        
-        return new_transition_times
-        
-    def forward(self, state, action=None):
+            
+    def forward(self, state, action):
         '''Update stage and transition times for already infected agents'''
         input_variables = self.input_variables
         t = state['current_step']
         
+        print("Substep: SEIRM progression!")
+        
         current_stages = get_by_path(state, re.split("/", input_variables['disease_stage']))
         current_transition_times = get_by_path(state, re.split("/", input_variables['next_stage_time']))
-        infected_to_recovered_time = get_by_path(state, re.split("/", input_variables['infected_to_recovered_time']))
 
-        new_stages = self.update_current_stages(t, current_stages, current_transition_times)
-        new_transition_times = self.update_times(t, new_stages, current_transition_times, infected_to_recovered_time)
+        new_stages = self.update_current_stages(t, current_stages, current_transition_times)        
+        new_transition_times = self.update_next_transition_times(t, new_stages, current_transition_times)
         
         return {self.output_variables[0]: new_stages, self.output_variables[1]: new_transition_times}
+        
