c08a6928ea873d24c338dfe5e01fc40af12e414f
Ayush Chopra <ayushchopra96@gmail.com>
2024-03-07 05:28:34 +0000

    simulate with quarantine intervention: success!

--- models/covid/simulator.py
+++ models/covid/simulator.py
@@ -11,11 +11,22 @@ from AgentTorch import Runner, Registry
 def get_registry():
     reg = Registry()
     
+    # Substep: New Disease Transmission
+    from substeps.new_transmission.transition import NewTransmission
+    reg.register(NewTransmission, "new_transmission", key="transition")
+
+    # Substep: Disease Stage Progression
     from substeps.seirm_progression.transition import SEIRMProgression
     reg.register(SEIRMProgression, "seirm_progression", key="transition")
 
-    from substeps.new_transmission.transition import NewTransmission
-    reg.register(NewTransmission, "new_transmission", key="transition")
+    # Substep: Quarantine Intervention
+    from substeps.quarantine.transition import UpdateQuarantineStatus
+    reg.register(UpdateQuarantineStatus, "update_quarantine_status", key="transition")
+    from substeps.quarantine.action import StartCompliance, BreakCompliance
+    reg.register(StartCompliance, "start_compliance", key="policy")
+    reg.register(BreakCompliance, "break_compliance", key="policy")
+    from substeps.quarantine.observation import GetFromState
+    reg.register(GetFromState, "get_from_state", key="observation")
          
     from substeps.utils import network_from_file, get_lam_gamma_integrals, get_mean_agent_interactions, get_infected_time, get_next_stage_time
     reg.register(network_from_file, "network_from_file", key="network")
--- models/covid/substeps/quarantine/transition.py
+++ models/covid/substeps/quarantine/transition.py
@@ -1,24 +1,27 @@
 import torch
 import torch.nn as nn
 import re
+import pdb
 
 from AgentTorch.substep import SubstepTransition
-from AgentTorch.helpers import discrete_sample, logical_and, logical_not, get_by_path
+from AgentTorch.helpers import discrete_sample, get_by_path, logical_and, logical_not
 
-class Quarantine(SubstepTransition):
+class UpdateQuarantineStatus(SubstepTransition):
     '''Logic: exposed or infected agents can start quarantine'''
     def __init__(self, config, input_variables, output_variables, arguments):
         super().__init__(config, input_variables, output_variables, arguments)
 
         self.device = self.config['simulation_metadata']['device']
-        self.num_agents = self.config['simulation_metatdata']['num_citizens']
+        self.num_agents = self.config['simulation_metadata']['num_citizens']
         self.quarantine_days = self.config['simulation_metadata']['quarantine_days']
-        self.num_steps = self.config['simulation_metadata']['num_steps']
+        self.num_steps = self.config['simulation_metadata']['num_steps_per_episode']
 
         self.SUSCEPTIBLE_VAR = self.config['simulation_metadata']['SUSCEPTIBLE_VAR']
         self.EXPOSED_VAR = self.config['simulation_metadata']['EXPOSED_VAR']
         self.INFECTED_VAR = self.config['simulation_metadata']['INFECTED_VAR']
         self.RECOVERED_VAR = self.config['simulation_metadata']['RECOVERED_VAR']
+        
+        self.INFINITY_TIME = self.config['simulation_metadata']['INFINITY_TIME']
 
         self.END_QUARANTINE_VAR = -1
         self.START_QUARANTINE_VAR = 1
@@ -26,58 +29,53 @@ class Quarantine(SubstepTransition):
 
     def _end_quarantine(self, t, is_quarantined, quarantine_start_date):
         agents_quarantine_end_date = quarantine_start_date + self.quarantine_days
-        agent_quarantine_ends =  (t>= agents_quarantine_end_date).long()
+        agent_quarantine_ends =  (t >= agents_quarantine_end_date).long()
 
-        is_quarantined += agent_quarantine_ends*self.END_QUARANTINE_VAR
-        quarantine_start_date = quarantine_start_date*(1 - agent_quarantine_ends) + (self.num_steps + 1)*agent_quarantine_ends
-
-        return is_quarantined, quarantine_start_date
-
-    def _start_quarantine(self, t, is_quarantined, exposed_infected_agents, quarantine_start_date, quarantine_start_prob):
-        agents_quarantine_start = discrete_sample(quarantine_start_prob,size=self.num_agents, device=self.device)
-        agents_quarantine_start = logical_and(agents_quarantine_start, exposed_infected_agents)
-        agents_quarantine_start = logical_and(logical_not(is_quarantined), agents_quarantine_start)
-
-        is_quarantined += agents_quarantine_start*self.START_QUARANTINE_VAR
-        quarantine_start_date = quarantine_start_date*(1 - agents_quarantine_start) + (agents_quarantine_start)*t
+        is_quarantined = is_quarantined + agent_quarantine_ends*self.END_QUARANTINE_VAR
+        quarantine_start_date = quarantine_start_date*(1 - agent_quarantine_ends) + (self.INFINITY_TIME)*agent_quarantine_ends
 
         return is_quarantined, quarantine_start_date
 
-    def _break_quarantine(self, t, is_quarantined, quarantine_start_date, quarantine_break_prob):
-        agents_quarantine_break = discrete_sample(quarantine_break_prob, size=self.num_agents, device=self.device)
-        agents_quarantine_break = logical_and(is_quarantined[t], agents_quarantine_break)
+    def _start_quarantine(self, t, is_quarantined, quarantine_start_date, agent_quarantine_start_action):
+        agents_quarantine_start = agent_quarantine_start_action.long()
 
-        is_quarantined += agents_quarantine_break*self.BREAK_QUARANTINE_VAR
-        quarantine_start_date = quarantine_start_date*(1 - agents_quarantine_break) + (self.num_steps + 1)*agents_quarantine_break
+        is_quarantined = is_quarantined + agents_quarantine_start*self.START_QUARANTINE_VAR
+        quarantine_start_date = quarantine_start_date*logical_not(agents_quarantine_start) + agents_quarantine_start*t
+#         quarantine_start_date = quarantine_start_date*(1 - agents_quarantine_start) + (agents_quarantine_start)*t
 
         return is_quarantined, quarantine_start_date
 
-    def update_quarantine_status(self, t, is_quarantined, infected_agents, quarantine_start_date, quarantine_start_prob, quarantine_break_prob):
+    def _break_quarantine(self, t, is_quarantined, quarantine_start_date, agent_quarantine_break_action):
+        agents_quarantine_break = agent_quarantine_break_action.long()
+
+        is_quarantined = is_quarantined + agents_quarantine_break*self.BREAK_QUARANTINE_VAR
+        quarantine_start_date = quarantine_start_date*logical_not(agents_quarantine_break) + agents_quarantine_break * self.INFINITY_TIME
+#         quarantine_start_date = quarantine_start_date*(1 - agents_quarantine_break) + (self.INFINITY_TIME)*agents_quarantine_break
+
+        return is_quarantined, quarantine_start_date
+
+    def update_quarantine_status(self, t, is_quarantined, quarantine_start_date, agent_quarantine_start_action, agent_quarantine_break_action):
         is_quarantined, quarantine_start_date = self._end_quarantine(t, is_quarantined, quarantine_start_date)
-        is_quarantined, quarantine_start_date = self._start_quarantine(t, is_quarantined, infected_agents, quarantine_start_date, quarantine_start_prob)
-        is_quarantined, quarantine_start_date = self._break_quarantine(t, is_quarantined, quarantine_start_date, quarantine_break_prob)
+        is_quarantined, quarantine_start_date = self._start_quarantine(t, is_quarantined, quarantine_start_date, agent_quarantine_start_action)
+        is_quarantined, quarantine_start_date = self._break_quarantine(t, is_quarantined, quarantine_start_date, agent_quarantine_break_action)
 
         return is_quarantined, quarantine_start_date
     
-    def forward(self, state, action=None):
+    def forward(self, state, action):
         input_variables = self.input_variables
         t = state['current_step']
+        print("t: ", t)
+        
+        print("Substep: Quarantine")
 
+        pdb.set_trace()
+        
         is_quarantined = get_by_path(state, re.split("/", input_variables['is_quarantined']))
         quarantine_start_date = get_by_path(state, re.split("/", input_variables['quarantine_start_date']))
-        disease_stage = get_by_path(state, re.split("/", input_variables['disease_stage']))
+                
+        agent_quarantine_start_action = action['citizens']['start_compliance_action']
+        agent_quarantine_break_action = action['citizens']['break_compliance_action']
 
-        quarantine_start_prob = self.args['quarantine_start_prob']
-        quarantine_break_prob = self.args['quarantine_break_prob']
-
-        # quarantine_start_prob = get_by_path(state, re.split("/", input_variables['quarantine_start_prob']))
-        # quarantine_break_prob = get_by_path(state, re.split("/", input_variables['quarantine_break_prob']))
-
-        not_susceptible = (disease_stage > self.SUSCEPTIBLE_VAR).long()
-        not_recovered = (disease_stage < self.RECOVERED_VAR).long()
-
-        infected_agents = logical_and(not_susceptible, not_recovered)
-
-        new_is_quarantined, new_quarantine_start_date = self.update_quarantine_status(self, t, is_quarantined, infected_agents, quarantine_start_date, quarantine_start_prob, quarantine_break_prob)
+        new_is_quarantined, new_quarantine_start_date = self.update_quarantine_status(t, is_quarantined, quarantine_start_date, agent_quarantine_start_action, agent_quarantine_break_action)
 
         return {self.output_variables[0]: new_is_quarantined, self.output_variables[1]: new_quarantine_start_date}
