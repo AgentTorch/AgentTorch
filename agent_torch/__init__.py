04f3b5e792d28e0edc5757ff78df2e48eb09132a
Ayush Chopra <ayushchopra96@gmail.com>
2024-06-11 22:39:04 +0000

    feat: differentiable agent_torch.distributions

--- agent_torch/__init__.py
+++ agent_torch/__init__.py
@@ -1,3 +1,5 @@
 from agent_torch.config import Configurator
 from agent_torch.registry import Registry
 from agent_torch.runner import Runner
+
+from agent_torch.helpers import distributions
--- agent_torch/helpers/__init__.py
+++ agent_torch/helpers/__init__.py
@@ -2,3 +2,5 @@ from agent_torch.helpers.general import *
 from agent_torch.helpers.environment import *
 from agent_torch.helpers.initializer import *
 from agent_torch.helpers.soft import *
+
+from agent_torch.helpers import distributions
