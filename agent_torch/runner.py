cb57620463ebe96f276772496b24fcb78252a80a
Ayush Chopra <ayushchopra96@gmail.com>
2024-06-11 19:13:59 +0000

    refc: remove trajectory(TODO: verify) + memory opt (save state to_cpu)

--- agent_torch/runner.py
+++ agent_torch/runner.py
@@ -1,19 +1,10 @@
-from omegaconf import OmegaConf
-import pandas as pd
-import torch
 import torch.nn as nn
 from collections import deque
-import argparse
-import re
-
-from abc import ABC, abstractmethod
 
 from agent_torch.controller import Controller
-from agent_torch.registry import Registry
 from agent_torch.initializer import Initializer
 
-from agent_torch.helpers import set_by_path
-
+from agent_torch.helpers import to_cpu
 
 class Runner(nn.Module):
     def __init__(self, config, registry) -> None:
@@ -30,13 +21,6 @@ class Runner(nn.Module):
 
         self.state = None
 
-        self.trajectory = {
-            "states": deque(),
-            "observations": deque(),
-            "actions": deque(),
-            "rewards": deque(),
-        }
-
     def init(self):
         r"""
         initialize the state of the simulation
@@ -45,7 +29,10 @@ class Runner(nn.Module):
         self.state = self.initializer.state
 
         self.state_trajectory = []
-        self.state_trajectory.append([self.state])
+        self.state_trajectory.append(
+            [to_cpu(self.state)]
+        )  # move state to cpu and save in trajectory
+
         for traj_var in self.trajectory.keys():
             self.trajectory[traj_var].append(deque())
 
@@ -61,12 +48,6 @@ class Runner(nn.Module):
         """
         self.state_trajectory = []
         self.state_trajectory.append([self.state])
-        self.trajectory = {
-            "states": deque(),
-            "observations": deque(),
-            "actions": deque(),
-            "rewards": deque(),
-        }
 
     def step(self, num_steps=None):
         r"""
@@ -84,11 +65,9 @@ class Runner(nn.Module):
 
         for time_step in range(num_steps):
             self.state["current_step"] = time_step
-            for traj_var in self.trajectory.keys():
-                self.trajectory[traj_var][-1].append(deque())
+
 
             for substep in self.config["substeps"].keys():
-                self.trajectory["states"][-1][-1].append(self.state)
                 observation_profile, action_profile = {}, {}
 
                 for agent_type in self.config["substeps"][substep]["active_agents"]:
@@ -105,21 +84,21 @@ class Runner(nn.Module):
                         agent_type,
                     )
 
-                self.trajectory["observations"][-1][-1].append(observation_profile)
-                self.trajectory["actions"][-1][-1].append(action_profile)
-
                 next_state = self.controller.progress(
                     self.state, action_profile, self.initializer.transition_function
                 )
                 self.state = next_state
 
-                self.state_trajectory[-1].append(self.state)
+                self.state_trajectory[-1].append(
+                    to_cpu(self.state)
+                )  # move state in state trajectory to cpu
 
     def _set_parameters(self, params):
+        print("_set_parameters CURRENTLY BREAKS GRADIENT! PLEASE DON'T USE")
         for param in params:
-            set_by_path(
-                root=self.state, items=re.split(".", param), value=params[param]
-            )
+            mode, param_name = param.split("/")[0], param.split("/")[1]
+            self.state[mode][param_name].data.copy_(params[param])
+            self.state[mode][param_name].requires_grad = True
 
     def step_from_params(self, num_steps=None, params=None):
         r"""
