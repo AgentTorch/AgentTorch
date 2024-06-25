cae9024bce17a37b2387a5c4ecf08a8c53f821c3
Vedant K <gamemaker0042@gmail.com>
2024-06-25 09:40:29 +0530

    meta: use relative imports in `__init__.py`

--- agent_torch/__init__.py
+++ agent_torch/__init__.py
@@ -1,10 +1,10 @@
-from agent_torch.config import Configurator
-from agent_torch.registry import Registry
-from agent_torch.runner import Runner
-from agent_torch.controller import Controller
-from agent_torch.initializer import Initializer
+from .config import Configurator
+from .registry import Registry
+from .runner import Runner
+from .controller import Controller
+from .initializer import Initializer
 
 from .version import __version__
 
 from .distributions import distributions
-from agent_torch.helpers.soft import *
+from .helpers.soft import *
