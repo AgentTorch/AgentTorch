e9aed72835de395280e07d9b51e7b5431096af9f
Shashank Kumar <39635214+sneakatyou@users.noreply.github.com>
2024-07-07 01:46:06 +0530

    fix

--- docs/tutorials/using-models/walkthrough.py
+++ docs/tutorials/using-models/walkthrough.py
@@ -1,32 +1,12 @@
-def debug():
-    import os
-    import sys
-
-    current_file_path = os.path.abspath(__file__)
-    current_directory = os.path.dirname(current_file_path)
-    package_root_directory = os.path.dirname(
-        os.path.dirname(os.path.dirname(current_directory))
-    )
-    sys.path.insert(0, package_root_directory)
-    sys.path.append(current_directory)
-
-
-debug()
-
 if __name__ == "__main__":
     from agent_torch.core.executor import Executor
     from agent_torch.core.dataloader import LinkPopulation
-    from agent_torch.populations import astoria
+    from agent_torch.populations import NYC
     from agent_torch.models import covid
 
-    nyc_population = LinkPopulation(astoria)  # all data for config.simulation_metadata
+    nyc_population = LinkPopulation(NYC)  # all data for config.simulation_metadata
     simulation = Executor(
         model=covid, pop_loader=nyc_population
-    )  # _update_config(), _get_runner()
+    ) 
 
     simulation.init()
-    # simulation.step(num_steps)
-    # simulation.optimize(num_episodes, num_steps_per_episode)
-
-    # # support:
-    # simulation.grads() - {[p.grad for p in runner.parameters()]}
