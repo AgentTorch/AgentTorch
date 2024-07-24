4663912ae859c62d8b321ca7537354b68e9448f7
Bhargav Shetgaonkar <67541365+BombayBrownBoy09@users.noreply.github.com>
2024-07-24 15:54:55 -0700

    Update prompt_manager.py
    Optimize runtime and efficiency of PromptManager

--- agent_torch/core/llm/prompt_manager.py
+++ agent_torch/core/llm/prompt_manager.py
@@ -2,10 +2,10 @@ import os
 import json
 import re
 import itertools
-
+from typing import List, Dict, Any, Tuple
 
 class PromptManager:
-    def __init__(self, user_prompt, population):
+    def __init__(self, user_prompt: str, population: Any):
         self.prompt = user_prompt
         self.population = population
         self.population_folder_path = self.population.population_folder_path
@@ -13,53 +13,38 @@ class PromptManager:
         self.mapping = self.load_mapping(mapping_path)
         self.variables = self.get_variables(self.prompt)
         self.filtered_mapping = self.filter_mapping(self.mapping, self.variables)
-        (
-            self.combinations_of_prompt_variables,
-            self.combinations_of_prompt_variables_with_index,
-        ) = self.get_combinations_of_prompt_variables(self.filtered_mapping)
+        self.combinations_of_prompt_variables = list(itertools.product(*self.filtered_mapping.values()))
+        self.combinations_of_prompt_variables_with_index = [
+            {k: v for k, v in zip(self.filtered_mapping.keys(), combo)} 
+            for combo in self.combinations_of_prompt_variables
+        ]
         self.distinct_groups = len(self.combinations_of_prompt_variables)
 
-    def load_mapping(self, path):
+    @staticmethod
+    def load_mapping(path: str) -> Dict[str, List[Any]]:
         with open(path, "r") as f:
-            mapping = json.load(f)
-        return mapping
+            return json.load(f)
 
-    def get_variables(self, prompt_template):
-        variables = re.findall(r"\{(.+?)\}", prompt_template)
-        return variables
+    @staticmethod
+    def get_variables(prompt_template: str) -> List[str]:
+        return re.findall(r"\{(.+?)\}", prompt_template)
 
-    def filter_mapping(self, mapping, variables):
+    @staticmethod
+    def filter_mapping(mapping: Dict[str, List[Any]], variables: List[str]) -> Dict[str, List[Any]]:
         return {k: mapping[k] for k in variables if k in mapping}
 
-    def get_combinations_of_prompt_variables(self, mapping):
-        combinations = list(itertools.product(*mapping.values()))
-        dict_combinations = [
-            dict(zip(mapping.keys(), combination)) for combination in combinations
-        ]
-        index_combinations = [
-            {k: mapping[k].index(v) for k, v in combination.items()}
-            for combination in dict_combinations
-        ]
-        return dict_combinations, index_combinations
+    def get_prompt_variables_dict(self, kwargs: Dict[str, Any]) -> Dict[str, Any]:
+        return {
+            key: getattr(self.population, key, kwargs.get(key))
+            for key in self.variables
+        }
 
-    def get_prompt_variables_dict(self, kwargs):
-        variables = {}
-        for key in self.variables:
-            variables[key] = (
-                getattr(self.population, key)
-                if hasattr(self.population, key)
-                else kwargs.get(key)
-            )
-        return variables
-
-    def get_prompt_list(self, kwargs):
-        self.dict_variables_with_values = self.get_prompt_variables_dict(kwargs=kwargs)
+    def get_prompt_list(self, kwargs: Dict[str, Any]) -> List[str]:
+        dict_variables_with_values = self.get_prompt_variables_dict(kwargs)
         prompt_list = []
-        for en, _ in enumerate(self.combinations_of_prompt_variables_with_index):
-            prompt_values = self.combinations_of_prompt_variables[en]
-            for key, value in self.dict_variables_with_values.items():
-                if isinstance(value, (int, str, float, bool)):
-                    prompt_values[key] = value
+        for combination in self.combinations_of_prompt_variables_with_index:
+            prompt_values = {**combination, **dict_variables_with_values}
             prompt = self.prompt.format(**prompt_values)
             prompt_list.append(prompt)
         return prompt_list
+    
