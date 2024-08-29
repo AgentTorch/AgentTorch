ca2aa1fd3f7b81b7f94690432dbe1352b59948fc
Vedant K <gamemaker0042@gmail.com>
2024-08-29 21:17:39 +0530

    fix: remove omega conf resolver warning

--- agent_torch/core/helpers/general.py
+++ agent_torch/core/helpers/general.py
@@ -101,7 +101,7 @@ def read_config(config_file, register_resolvers=True):
 
         for name, func in resolvers:
             try:
-                OmegaConf.register_resolver(name, func)
+                OmegaConf.register_new_resolver(name, func)
             except AssertionError as e:
                 if "is already registered" in str(e):
                     continue
